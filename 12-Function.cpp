81c4e7a01fe934bc49d3b4f882c6bf5ea174f822
Abolfazl-Azadegan <Abolfazl.azadegan@yahoo.com>
2025-05-07 14:07:02 +0330

    define function

---
+++ 12-Function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+
+int sum(int first_number , int second_number){
+    int result = first_number + second_number;
+    return result;
+}
+
+int main(){
+    
+    int fir_num {10};
+    int sec_num {20};
+
+    std::cout << "First Number is: " << fir_num << std::endl;
+    std::cout << "Second Number is: " << sec_num << std::endl;
+    std::cout << "Sum of two numbers is: " << sum(fir_num, sec_num) << std::endl;
+}
+
